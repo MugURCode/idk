91ea4e8c220424beb1c7039cb6f7021e0e2a53da
SamiFawcett <samif7466@afsenyc.org>
2017-08-07 13:49:56 -0400

    here

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var database = firebase.database().ref();
+
+function updateDatabase(){
+    var answer = $("#answer").val();
+
+    var newValue = {
+        USERANSWER: answer,
+        COMPANSWER: compChoose()
+    }
+
+    database.push(newValue, function(){
+        console.log("success");
+    });
+}
+
+database.on("child_added", function(data){
+    var row = data.val();
+    console.log(row);
+    var userAnswer = row.USERANSWER;
+    if(filter(userAnswer)){
+        var displayText = "<p id='userAnswer'> " + userAnswer + " </p>";
+        $("#main-frame").append(displayText);
+        run(userAnswer, row.COMPANSWER);
+    } else {
+        $("#main-frame").append("<p> " + userAnswer + " isn't an option</p>")
+    }
+    
+});
+
+function compChoose(){
+    var possibleAnswers = ["Rock", "Paper", "Scissors"];
+    var compAnswer = possibleAnswers[Math.floor(Math.random() * 3)];
+    return compAnswer;
+}
+
+function filter(word){
+    var keywords = ["rock", "paper", "scissors"];
+    for(var i = 0; i < keywords.length; i++){
+       if(word.toLowerCase() === keywords[i]){
+            return true;
+       }
+    }
+    return false;
+}
+
+function run(userAnswer, compAnswer){
+    if(compAnswer === "Rock"){
+        switch(userAnswer){
+            case "rock":
+            case "paper":
+            case "scissors":
+        }
+    }
+}
+
+if(performance.navigation.type == 1){
+    $("#main-frame").empty();
+}
